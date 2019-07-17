a5a107d5d619a538b62942581975fe1ed5900620
divyanshpuri02 <divyanshpuri02@gmail.com>
2019-07-17 11:07:21 +0530

    Add files via upload

---
+++ COCO_2018-Stuff-Segmentation-Challenge/scripts/script.sh
@@ -0,0 +1,20 @@
+pip install --upgrade pip
+pip install -U protobuf
+pip uninstall -y tensorflow tensorflow-tensorboard
+pip install keras
+pip install matplotlib
+pip install image
+pip install pandas
+pip install opencv-python
+pip install opencv-contrib-python
+apt-get install -y libsm6 libxext6 libxrender-dev
+pip install imgaug h5py
+pip install numpy
+pip install scipy
+pip install Pillow
+pip install cython
+pip install matplotlib
+pip install scikit-image
+pip install h5py
+ipip install imgaug
+pip install tf-nightly-gpu
